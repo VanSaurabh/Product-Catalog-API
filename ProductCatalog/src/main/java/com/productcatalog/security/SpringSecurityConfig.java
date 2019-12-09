aa758499b829b27f178c6f1f8b4f45cd7334e983
Saurabh Kundu <saurabh.kundu93@gmail.com>
2019-12-10 00:20:33 +0530

    Added swagger in the existing apis.

--- ProductCatalog/src/main/java/com/productcatalog/security/SpringSecurityConfig.java
+++ ProductCatalog/src/main/java/com/productcatalog/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,6 +18,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String USER = "USER";
 	private static final String ADMIN = "ADMIN";
 
+	private static final String[] AUTH_WHITELIST = {
+			"**/swagger-resources/**",
+	        "/swagger-ui.html",
+	        "/v2/api-docs",
+	        "/webjars/**"
+    };
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("password").roles(USER).and().withUser("admin")
@@ -28,18 +35,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers(AUTH_WHITELIST);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/v1/categories").hasAnyRole(USER, ADMIN)
-				.antMatchers(HttpMethod.POST, "/v1/categories").hasRole(ADMIN)
-				.antMatchers(HttpMethod.PUT, "/v1/categories/**").hasRole(ADMIN)
-				.antMatchers(HttpMethod.DELETE, "/v1/categories/**").hasRole(ADMIN)
-				.antMatchers(HttpMethod.GET, "/v1/products").hasAnyRole(USER, ADMIN)
-				.antMatchers(HttpMethod.POST, "/v1/products").hasRole(ADMIN)
-				.antMatchers(HttpMethod.PUT, "/v1/products/**").hasRole(ADMIN)
-				.antMatchers(HttpMethod.DELETE, "/v1/products/**").hasRole(ADMIN).and().httpBasic().and().csrf()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "api/v1/categories").hasAnyRole(USER, ADMIN)
+				.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+				.antMatchers(HttpMethod.POST, "api/v1/categories").hasRole(ADMIN)
+				.antMatchers(HttpMethod.PUT, "api/v1/categories/**").hasRole(ADMIN)
+				.antMatchers(HttpMethod.DELETE, "api/v1/categories/**").hasRole(ADMIN)
+				.antMatchers(HttpMethod.GET, "api/v1/products").hasAnyRole(USER, ADMIN)
+				.antMatchers(HttpMethod.POST, "api/v1/products").hasRole(ADMIN)
+				.antMatchers(HttpMethod.PUT, "api/v1/products/**").hasRole(ADMIN)
+				.antMatchers(HttpMethod.DELETE, "api/v1/products/**").hasRole(ADMIN).and().httpBasic().and().csrf()
 				.disable().formLogin().disable().headers().frameOptions().disable();
 	}
 }
